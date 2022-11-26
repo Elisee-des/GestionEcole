f6218c3320fd1a69b1ff92c996077a0e81e5d70f
Elisee-dev <esabidani@gmail.com>
2022-11-26 22:42:57 +0000

    CRUD pour l'anneeScolaire ok

---
+++ src/Controller/Admin/AnneeScolaireController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\AnneeScolaire;
+use App\Form\AnneeScolaireType;
+use App\Form\EditerAnneeScolaireType;
+use App\Repository\AnneeScolaireRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("admin/annee_scolaire", name="admin_annee_scolaire_")
+ */
+class AnneeScolaireController extends AbstractController
+{
+    /**
+     * @Route("/", name="liste")
+     */
+    public function index(AnneeScolaireRepository $anneeScolaireRepository): Response
+    {
+        $anneeScolaires = $anneeScolaireRepository->findAll();
+
+        return $this->render('admin/annee_scolaire/index.html.twig', [
+            'anneescolaires' => $anneeScolaires,
+        ]);
+    }
+
+    /**
+     * @Route("/creer", name="creer")
+     */
+    public function creer(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $anneeScolaire = new AnneeScolaire();
+
+        $form = $this->createForm(AnneeScolaireType::class, $anneeScolaire);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $annee = $request->get("annee_scolaire")["annee"];
+            $description = $request->get("annee_scolaire")["description"];
+
+            $anneeScolaire->setAnnee($annee);
+            $anneeScolaire->setDescription($description);
+
+            $entityManager->persist($anneeScolaire);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                "Annee " . $anneeScolaire->getAnnee() . " a ete ajouter avec succes"
+            );
+
+            return $this->redirectToRoute('admin_annee_scolaire_liste');
+        }
+
+        return $this->render('admin/annee_scolaire/creer.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/editer/{id}", name="editer")
+     */
+    public function editer(EntityManagerInterface $entityManager, Request $request, AnneeScolaire $anneeScolaire): Response
+    {
+
+        $form = $this->createForm(EditerAnneeScolaireType::class, $anneeScolaire);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $annee = $request->get("editer_annee_scolaire")["annee"];
+            $description = $request->get("editer_annee_scolaire")["description"];
+
+            $anneeScolaire->setAnnee($annee);
+            $anneeScolaire->setDescription($description);
+
+            $entityManager->persist($anneeScolaire);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                "Vous avez modifié avec succes une annee. La nouvelle annee est " . $anneeScolaire->getAnnee() . " a ete ajouter avec succes"
+            );
+
+            return $this->redirectToRoute('admin_annee_scolaire_liste');
+        }
+
+        return $this->render('admin/annee_scolaire/creer.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+     /**
+     * @Route("/supprimer/{id}", name="supprimer")
+     */
+    public function supprimer(EntityManagerInterface $entityManager, AnneeScolaire $anneeScolaire): Response
+    {
+
+            $entityManager->remove($anneeScolaire);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                "Annee scolaire supprimer avec succes"
+            );
+
+            return $this->redirectToRoute('admin_annee_scolaire_liste');
+
+    }
+}
---
+++ src/Form/AnneeScolaireType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\AnneeScolaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnneeScolaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('annee', NumberType::class)
+            ->add('description', TextareaType::class)
+            ->add('Creer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AnneeScolaire::class,
+        ]);
+    }
+}
